63fb289e7c1a01ec46f57bfce9d20a22577452cb
samuelperezfente <samuel.perez.fente@udc.es>
2025-03-03 20:17:08 +0100

    Añadido el mapa del ayuntamiento

---
+++ Mapas/Objetos/Objetos.tsx
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Puertas/Ventanas" tilewidth="96" tileheight="144" tilecount="49" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="PuertaCerrada.png" width="80" height="96"/>
+ </tile>
+ <tile id="1">
+  <image source="PuertaAbierta.png" width="95" height="95"/>
+ </tile>
+ <tile id="2">
+  <image source="PuertaAbierta2.png" width="95" height="95"/>
+ </tile>
+ <tile id="8">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_57.png" width="96" height="96"/>
+ </tile>
+ <tile id="47">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_55.png" width="96" height="96"/>
+ </tile>
+ <tile id="9">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_37.png" width="96" height="144"/>
+ </tile>
+ <tile id="10">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_18.png" width="96" height="128"/>
+ </tile>
+ <tile id="74">
+  <image source="Museum_Black_Shadow_Singles_48x48_197.png" width="96" height="144"/>
+ </tile>
+ <tile id="73">
+  <image source="Museum_Black_Shadow_Singles_48x48_196.png" width="96" height="112"/>
+ </tile>
+ <tile id="72">
+  <image source="Museum_Black_Shadow_Singles_48x48_195.png" width="48" height="80"/>
+ </tile>
+ <tile id="12">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_87.png" width="48" height="128"/>
+ </tile>
+ <tile id="76">
+  <image source="LivingRoom_Black_Shadow_Singles_48x48.png" width="53" height="57"/>
+ </tile>
+ <tile id="48">
+  <image source="Living_Room_Black_Shadow_Singles_48x48_59.png" width="96" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="Classroom_and_Library_Singles_48x48_43.png" width="96" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="Music_and_Sport_Black_Shadow_Singles_48x48_108.png" width="96" height="80"/>
+ </tile>
+ <tile id="17">
+  <image source="Music_and_Sport_Black_Shadow_Singles_48x48_163.png" width="96" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="Music_and_Sport_Black_Shadow_Singles_48x48_164.png" width="48" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="Music_and_Sport_Black_Shadow_Singles_48x48_215.png" width="96" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="Art_Shadow_Singles_48x48_45.png" width="48" height="80"/>
+ </tile>
+ <tile id="21">
+  <image source="Art_Shadow_Singles_48x48_43.png" width="48" height="80"/>
+ </tile>
+ <tile id="68">
+  <image source="Museum_Black_Shadow_Singles_48x48_94.png" width="48" height="80"/>
+ </tile>
+ <tile id="69">
+  <image source="Museum_Black_Shadow_Singles_48x48_97.png" width="48" height="80"/>
+ </tile>
+ <tile id="70">
+  <image source="Museum_Black_Shadow_Singles_48x48_103.png" width="48" height="80"/>
+ </tile>
+ <tile id="71">
+  <image source="Museum_Black_Shadow_Singles_48x48_106.png" width="48" height="80"/>
+ </tile>
+ <tile id="22">
+  <image source="Basement_Shadow_Singles_48x48_45.png" width="80" height="80"/>
+ </tile>
+ <tile id="23">
+  <image source="Basement_Shadow_Singles_48x48_46.png" width="80" height="48"/>
+ </tile>
+ <tile id="24">
+  <image source="Basement_Shadow_Singles_48x48_47.png" width="80" height="48"/>
+ </tile>
+ <tile id="61">
+  <image source="Basement_Shadow_Singles_48x48_50.png" width="80" height="48"/>
+ </tile>
+ <tile id="59">
+  <image source="Basement_Shadow_Singles_48x48_48.png" width="80" height="80"/>
+ </tile>
+ <tile id="60">
+  <image source="Basement_Shadow_Singles_48x48_49.png" width="80" height="48"/>
+ </tile>
+ <tile id="31">
+  <image source="Basement_Shadow_Singles_48x48_207.png" width="96" height="96"/>
+ </tile>
+ <tile id="32">
+  <image source="Basement_Shadow_Singles_48x48_3.png" width="96" height="80"/>
+ </tile>
+ <tile id="30">
+  <image source="Basement_Shadow_Singles_48x48_238.png" width="96" height="96"/>
+ </tile>
+ <tile id="36">
+  <image source="Museum_Black_Shadow_Singles_48x48_115.png" width="96" height="80"/>
+ </tile>
+ <tile id="37">
+  <image source="Museum_Black_Shadow_Singles_48x48_143.png" width="48" height="128"/>
+ </tile>
+ <tile id="38">
+  <image source="Museum_Black_Shadow_Singles_48x48_144.png" width="48" height="48"/>
+ </tile>
+ <tile id="39">
+  <image source="Museum_Black_Shadow_Singles_48x48_145.png" width="48" height="48"/>
+ </tile>
+ <tile id="40">
+  <image source="Television_and_Film_Studio_Black_Shadow_Singles_48x48_31.png" width="96" height="80"/>
+ </tile>
+ <tile id="44">
+  <image source="Hospital_Black_Shadow_Singles_48x48_15.png" width="48" height="128"/>
+ </tile>
+ <tile id="52">
+  <image source="Hospital_Black_Shadow_Singles_48x48_17.png" width="48" height="80"/>
+ </tile>
+ <tile id="45">
+  <image source="Hospital_Black_Shadow_Singles_48x48_19.png" width="96" height="80"/>
+ </tile>
+ <tile id="46">
+  <image source="Hospital_Black_Shadow_Singles_48x48_18.png" width="48" height="128"/>
+ </tile>
+ <tile id="51">
+  <image source="Hospital_Black_Shadow_Singles_48x48_20.png" width="48" height="128"/>
+ </tile>
+ <tile id="66">
+  <image source="Hospital_Black_Shadow_Singles_48x48_32.png" width="48" height="128"/>
+ </tile>
+ <tile id="67">
+  <image source="Hospital_Black_Shadow_Singles_48x48_33.png" width="96" height="80"/>
+ </tile>
+ <tile id="65">
+  <image source="Hospital_Black_Shadow_Singles_48x48_29.png" width="48" height="128"/>
+ </tile>
+ <tile id="53">
+  <image source="Hospital_Black_Shadow_Singles_48x48_49.png" width="96" height="80"/>
+ </tile>
+ <tile id="54">
+  <image source="Hospital_Black_Shadow_Singles_48x48_44.png" width="48" height="32"/>
+ </tile>
+ <tile id="56">
+  <image source="Hospital_Black_Shadow_Singles_48x48_40.png" width="48" height="32"/>
+ </tile>
+</tileset>
--- ayuntamiento.py
+++ ayuntamiento.py
@@ -1,3 +1,5 @@
+import math
+import numpy
 import pygame
 import pytmx
 
@@ -7,14 +9,32 @@ from settings import *
 
 class Ayuntamiento:
     def __init__(self):
-        self.tmxdata = pytmx.load_pygame("Mapas/ayuntamiento48x48.tmx")
+        self.tmxdata = pytmx.load_pygame("Mapas/ayuntamiento48x48v2.tmx")
 
         self.width = self.tmxdata.width * self.tmxdata.tilewidth
         self.height = self.tmxdata.height * self.tmxdata.tileheight
         self.grupoObstaculos = pygame.sprite.Group()
+        self.grupoObjetos = pygame.sprite.Group()
 
-        for tile_object in self.tmxdata.objects:
-            self.grupoObstaculos.add(Obstacle(pygame.Rect(tile_object.x, tile_object.y, tile_object.width, tile_object.height)))
+        for objectGroup in self.tmxdata.objectgroups:
+            if objectGroup.name == "Obstáculos":
+                for object in objectGroup:
+                    self.grupoObstaculos.add(Obstacle(pygame.Rect(object.x, object.y, object.width, object.height)))
+            elif objectGroup.name == "PuertasAbiertas":
+                for object in objectGroup: 
+                    
+                    print(object.image)  
+                    obj = Object(pygame.Rect(object.x, object.y, object.width, object.height), object.image)
+                    self.grupoObjetos.add(obj)
+            else:
+                for object in objectGroup: 
+
+                    print(object.image)  
+                    obj = Object(pygame.Rect(object.x, object.y, object.width, object.height), object.image)
+                    self.grupoObstaculos.add(obj)
+                    self.grupoObjetos.add(obj)
+            # print(dir(tile_object))
+            # self.grupoObstaculos.add(Obstacle(pygame.Rect(tile_object.x, tile_object.y, tile_object.width, tile_object.height)))
 
         
 
@@ -27,11 +47,16 @@ class Ayuntamiento:
                     tile = ti(gid)
                     
                     if tile:
-                        pantalla.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))       
+                        pantalla.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))     
+
+        
+
+
     
     def make_map(self):
         temp_surface = pygame.Surface((self.width, self.height)).convert()
         self.dibujar(temp_surface)
+        self.grupoObjetos.draw(temp_surface)
         return temp_surface
     
 
@@ -44,4 +69,13 @@ class Obstacle(MiSprite):
         # Y lo situamos de forma global en esas coordenadas
         self.establecerPosicion((self.rect.left, self.rect.bottom))
 
-        # self.image = pygame.Surface((0, 0))
+class Object(MiSprite):
+    def __init__(self,rectangulo, imagen):
+        # Primero invocamos al constructor de la clase padre
+        MiSprite.__init__(self)
+        # Rectangulo con las coordenadas en pantalla que ocupara
+        self.rect = rectangulo
+        # Y lo situamos de forma global en esas coordenadas
+        self.establecerPosicion((self.rect.left, self.rect.bottom))
+        
+        self.image = imagen
